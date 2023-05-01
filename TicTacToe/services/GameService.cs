828099507ffea3bdb43160afccc010d08e6c5d29
Daniel Miller <daniel9miller6@gmail.com>
2023-05-02 00:40:34 +0100

    Assert order of game launch operations

--- TicTacToe/services/GameService.cs
+++ TicTacToe/services/GameService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TicTacToe.Interfaces;
+using TicTacToe.Models;
 
 namespace TicTacToe.Services
 {
@@ -18,7 +19,11 @@ namespace TicTacToe.Services
 
         public void LaunchGame()
         {
+            userInterface.IntroduceGame();
             userInterface.EstablishPlayerIdentity();
+            var grid = new Grid();
+            userInterface.PresentLatestGrid(grid);
+            userInterface.GetNextMove();
         }
     }
 }
